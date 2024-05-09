461c3afc4892b6d5e4cb55f60c8ac81b764d7fc0
rsbickmore <155321605+rsbickmore@users.noreply.github.com>
2024-05-09 15:30:24 -0700

    Added component files, updated app.js

--- capstone-project/src/App.js
+++ capstone-project/src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      Homepage
-    </div>
+    <>
+      <header></header>
+      <nav></nav>
+      <main></main>
+      <footer></footer>
+    </>
   );
 }
 
