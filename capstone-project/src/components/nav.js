671cd797d135eac8b3771d08db78834145a1ed0a
rsbickmore <155321605+rsbickmore@users.noreply.github.com>
2024-05-09 16:57:09 -0700

    added mobile navbar to nav.js

--- capstone-project/src/components/nav.js
+++ capstone-project/src/components/nav.js
@@ -7,6 +7,13 @@ const Nav = () => {
             <a href='/'>
                 <img src={logo} alt='logo'/>
             </a>
+
+            <div>
+                <div className='bar'></div> {/*go to 4:05 in the video for css*/}
+                <div className='bar'></div>
+                <div className='bar'></div>
+            </div>
+
         </nav>
     );
 };
