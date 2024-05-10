392605111c72113888bdd0a50023aa4b83d372d2
rsbickmore <155321605+rsbickmore@users.noreply.github.com>
2024-05-10 07:25:14 -0700

    styled navbar

--- capstone-project/src/components/nav.js
+++ capstone-project/src/components/nav.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../images/logo-sm.jpg'
 
 const Nav = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () =>{
+        setMenuOpen(!menuOpen);
+    }
+
     return (
-        <nav>
-            <a href='/'>
-                <img src={logo} alt='logo'/>
+        <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+            <a href='/' className="logo">
+                <img src={logo} width={210} height={58} alt="logo" />
             </a>
 
-            <div>
-                <div className='bar'></div> {/*go to 4:05 in the video for css*/}
-                <div className='bar'></div>
-                <div className='bar'></div>
+            {/*mobile nav bar */}
+            <div className="menu-icon" onClick={toggleMenu}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
             </div>
 
+            <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
+                <li>
+                    <a href='/'>Home</a>
+                </li>
+                <li>
+                    <a href='/'>About</a>
+                </li>
+                <li>
+                    <a href='/'>Menu</a>
+                </li>
+                <li>
+                    <a href='/'>Reservations</a>
+                </li>
+                <li>
+                    <a href='/'>Order Online</a>
+                </li>
+                <li>
+                    <a href='/'>Login</a>
+                </li>
+            </ul>
         </nav>
     );
 };
